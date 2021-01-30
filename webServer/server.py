86acb7735cb066e27ffadd774c6b2ac7c397da26
Deepak <joshideepak4598@gmail.com>
2021-01-30 18:42:48 +0530

    Send notification when server is down

--- webServer/server.py
+++ webServer/server.py
@@ -1,10 +1,52 @@
-import time
 import os
+import time
+import smtplib
 import mysql.connector
+from flask_mail import Message , Mail
 from subprocess import Popen, TimeoutExpired, PIPE
 
 up = []
 down = []
+
+def sendEmail(ip,name):
+    print("Sending email for ip ",ip," and name ",name)
+    mydb = mysql.connector.connect(host="127.0.0.1",user="root",password="",database="drashti")
+    mycursor = mydb.cursor()
+
+    mycursor.execute("SELECT email FROM notification")
+    data = mycursor.fetchall()
+    mycursor.close()
+    mycursor = mydb.cursor()
+    mydb.close()
+
+    emails = [email[0] for email in data]
+    gmail_user = 'drashtimonitoringtool@gmail.com'
+    gmail_password = 'testPassword'
+
+    sent_from = gmail_user
+    to = emails
+    subject = name + ' is Down'
+    body = 'You machine : ' + name + " with ip " + ip + " just went down."
+
+    email_text = """\
+    From: %s
+    To: %s
+    Subject: %s
+
+    %s
+    """ % (sent_from, ", ".join(to), subject, body)
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(gmail_user, gmail_password)
+        server.sendmail(sent_from, to, email_text)
+        server.close()
+        print('Email sent!')
+    except:
+        print('Something went wrong...')
+
+
 
 def scanip(ip):
     cmd = "ping -c 1 " + ip + " -W 1000 > /dev/null"
@@ -45,12 +87,12 @@ def runI():
     mycursor = mydb.cursor()
 
     mycursor.execute("SELECT ip FROM nodes")
-    result = mycursor.fetchall()
+    data = mycursor.fetchall()
     mycursor.close()
     mycursor = mydb.cursor()
     mydb.close()
 
-    result = [x[0] for x in result]
+    result = [x[0] for x in data]
     print("Checking ",result)
     for ip in result:
         if scanip(ip) == "1":
@@ -73,6 +115,18 @@ def runI():
         if newDownIps:
             for ip in newDownIps:
                 changeStatus(0,ip)
+                mydb = mysql.connector.connect(host="127.0.0.1",user="root",password="",database="drashti")
+                mycursor = mydb.cursor()
+                mycursor.execute("SELECT ip,name FROM nodes WHERE server=1 AND status=0")
+                downServers = mycursor.fetchall()
+                mycursor.close()
+                mycursor = mydb.cursor()
+                mydb.close()
+
+                for record in list(downServers):
+                    if record[0] in newDownIps:
+                        sendEmail(record[0],record[1])
+
 
     up = newUp
     down = newDown
